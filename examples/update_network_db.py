8822874248f7b0f4f9e1e4b04e0e3a37220088b1
jianzhnie <jianzhnie@126.com>
2024-08-08 09:38:58 +0800

    Updata CyberAttackSim

--- examples/update_network_db.py
+++ examples/update_network_db.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
+import networkx as nx
+
 sys.path.append(os.getcwd())
 from cyberattacksim.networks import network_creator
 from cyberattacksim.networks.network_db import NetworkDB
-
-description = 'A randomly connected graph. With the guarantee that each node will have at least one connection.'
-author = 'Robin/CyberAttack'
-base_name = 'Randomly Connected Network'
+from cyberattacksim.utils.env_utils import (
+    get_network_from_edges_and_positions, read_nodes_edges_from_file)
 
 if __name__ == '__main__':
     db = NetworkDB()
@@ -66,6 +66,39 @@ if __name__ == '__main__':
     author = 'Robion/CyberAttack'
     base_name = 'Corporate Network'
     network = network_creator.create_corporate_network()
+    name = base_name
+    db.upsert(network,
+              name=name,
+              description=description,
+              author=author,
+              locked=True)
+
+    # Craete Real Network
+    current_dir = os.getcwd()
+    # directories
+    log_dir = os.path.join(current_dir, 'work_dir', 'random_nodes_logs_dir')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    edges_file = os.path.join(log_dir, 'random_nodes.edgelist')
+    G = nx.karate_club_graph()
+    pos = nx.spring_layout(G, iterations=100, seed=42)
+
+    nx.write_edgelist(G, edges_file)
+    nodes, edges = read_nodes_edges_from_file(edges_file)
+    network = get_network_from_edges_and_positions(edges, pos)
+
+    network.set_random_entry_nodes = True
+    network.num_of_random_entry_nodes = 3
+    network.reset_random_entry_nodes()
+    network.set_random_high_value_nodes = True
+    network.num_of_random_high_value_nodes = 3
+    network.reset_random_high_value_nodes()
+    network.set_random_vulnerabilities = True
+    network.reset_random_vulnerabilities()
+
+    description = 'Zachary’s Karate Club graph.'
+    author = 'Robion/CyberAttack'
+    base_name = 'Karate Club Network'
     name = base_name
     db.upsert(network,
               name=name,

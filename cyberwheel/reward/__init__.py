eea7ff37aa42ea47f86c5f918922b88bcc487607
jianzhnie <jianzhnie@126.com>
2024-08-27 16:05:00 +0800

    add cyberwheel rewards

--- cyberwheel/reward/__init__.py
+++ cyberwheel/reward/__init__.py
@@ -1,9 +1,9 @@
-from .step_detected_reward import StepDetectedReward
-from .isolate_reward import IsolateReward
 from .decoy_reward import DecoyReward
+from .isolate_reward import IsolateReward
 from .recurring_reward import RecurringReward
 from .restore_reward import RestoreReward
 from .reward_base import RewardMap
+from .step_detected_reward import StepDetectedReward
 
 __all__ = [
     "StepDetectedReward",

7069fb76ba172ad5ef1a6da169c57225d7172264
jianzhnie <jianzhnie@126.com>
2024-08-27 15:53:33 +0800

    add cyberwheel envs

---
+++ cyberwheel/envs/cyberwheel.py
@@ -0,0 +1,24 @@
+from cyberwheel.network.network_base import Network
+
+
+class Cyberwheel:
+
+    def __init__(self, **kwargs):
+        # use config_file_path kwarg if supplied
+        self.config_file_path = kwargs.get('config_file_path')
+
+        # self.network = RandomNetwork(number_hosts,number_subnets,connect_subnets_probability)
+        # print("begin reading")
+        net = kwargs.get('network', None)
+        if net is None:
+            print('creating from yaml')
+            self.network = Network.create_network_from_yaml(
+                self.config_file_path)
+        else:
+            self.network = net
+        # self.network_copy = copy(self.network)
+        self.evaluation = False
+
+    @classmethod
+    def create_from_yaml(cls, config_file_path):
+        return cls(config_file_path=config_file_path)
---
+++ cyberwheel/envs/observation/observation.py
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+from typing import Iterable
+
+import numpy as np
+
+from cyberwheel.detectors.alert import Alert
+from cyberwheel.network.host import Host
+from cyberwheel.network.network_base import Network
+
+
+class Observation:
+    """
+    A base class for converting from detector produced alerts to blue observations.
+    Hopefully, this can be used to create different observation vectors easily.
+    """
+
+    @abstractmethod
+    def create_obs_vector(self, alerts: Iterable[Alert]) -> Iterable:
+        """create_obs_vector() maps alerts to the blue observation space represented by a vector"""
+        pass
+
+    def reset_obs_vector(self) -> Iterable:
+        """Resets the obs_vector and returns the observation of the initial state"""
+
+    def set_network(self, network: Network) -> None:
+        self.network = network
+
+
+class TestObservation(Observation):
+
+    def create_obs_vector(self, alerts: Iterable) -> Iterable:
+        num_hosts = sum(
+            isinstance(data_object, Host)
+            for _, data_object in self.network.graph.nodes(data="data"))
+        observation_vector = np.zeros(num_hosts, dtype=np.int8)
+        index = 0
+        for _, data_object in self.network.graph.nodes(data="data"):
+            if not isinstance(data_object, Host):
+                continue
+            for alert in alerts:
+                if data_object in alert.dst_hosts:
+                    observation_vector[index] = 1
+            index += 1
+        return observation_vector

66b6182fc0e5cc346951223a65769737d6bc7744
jianzhnie <jianzhnie@126.com>
2024-08-30 15:10:27 +0800

    Updata cyberwheel detectors

---
+++ cyberwheel/detectors/__init__.py
@@ -0,0 +1,13 @@
+from cyberwheel.detectors.alert import Alert
+from cyberwheel.detectors.detector_base import Detector
+from cyberwheel.detectors.handler import DetectorHandler
+from cyberwheel.detectors.isolate_detector import IsolateDetector
+from cyberwheel.detectors.probability_detector import ProbabilityDetector
+
+__all__ = [
+    'Alert',
+    'Detector',
+    'DetectorHandler',
+    'IsolateDetector',
+    'ProbabilityDetector',
+]

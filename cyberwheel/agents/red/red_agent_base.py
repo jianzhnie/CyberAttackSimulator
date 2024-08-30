0f6e7e2d64d54687304dd34155c866a9eb98c551
jianzhnie <jianzhnie@126.com>
2024-08-30 10:16:31 +0800

    Update red_agent_base.py

--- cyberwheel/agents/red/red_agent_base.py
+++ cyberwheel/agents/red/red_agent_base.py
@@ -4,8 +4,7 @@ from ipaddress import IPv4Address, IPv6Address
 from typing import Any, List, Type
 
 from cyberwheel.agents.red.actions import ARTAction, RedActionResults
-from cyberwheel.agents.red.actions.art_killchain_phases import \
-    ARTKillChainPhase
+from cyberwheel.agents.red.actions import ARTKillChainPhase
 from cyberwheel.network.network_base import Host
 from cyberwheel.network.service import Service
 from cyberwheel.reward import RewardMap

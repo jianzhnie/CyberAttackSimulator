eeb81ff8f268a84801bc8164a7ccd14b062b80b9
jianzhnie <jianzhnie@126.com>
2024-08-31 15:56:10 +0800

    Update __init__.py

--- cyberwheel/detectors/__init__.py
+++ cyberwheel/detectors/__init__.py
@@ -3,11 +3,26 @@ from cyberwheel.detectors.detector_base import Detector
 from cyberwheel.detectors.handler import DetectorHandler
 from cyberwheel.detectors.isolate_detector import IsolateDetector
 from cyberwheel.detectors.probability_detector import ProbabilityDetector
+from cyberwheel.detectors.example_detectors import (
+    CoinFlipDetector,
+    DecoyDetector,
+    PerfectDetector,
+)
+
 
 __all__ = [
-    'Alert',
-    'Detector',
-    'DetectorHandler',
-    'IsolateDetector',
-    'ProbabilityDetector',
+    "Alert",
+    "Detector",
+    "DetectorHandler",
+    "IsolateDetector",
+    "ProbabilityDetector",
+    "CoinFlipDetector",
+    "DecoyDetector",
+    "PerfectDetector",
 ]
+
+detetctor_maping = {
+    "CoinFlipDetector": CoinFlipDetector,
+    "DecoyDetector": DecoyDetector,
+    "PerfectDetector": PerfectDetector,
+}

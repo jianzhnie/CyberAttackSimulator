4c5e5c85c6eee255376136a986b928015f0ea6eb
李天乐 <litl@pcl.ac.cn>
2024-09-08 12:34:44 +0800

    更新 'cyberattacksim/envs/generic/core/blue_interface.py'

--- cyberattacksim/envs/generic/core/blue_interface.py
+++ cyberattacksim/envs/generic/core/blue_interface.py
@@ -131,7 +131,8 @@ class BlueInterface(BlueActionSet):
             action = action - self.deceptive_actions
             # global actions
             if action < self.number_global_action:
-                blue_action, blue_node = self.global_action_dict[action]()
+                # blue_action, blue_node = self.global_action_dict[action]()   # source version
+				blue_action, blue_node = self.global_action_dict[int(action)]()  # changed by ltl
             else:
                 # standard actions
                 action = action - self.number_global_action

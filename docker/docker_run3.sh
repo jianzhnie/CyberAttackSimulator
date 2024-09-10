15ad187125cd4fcd3c8762ebd75bf4c6b03368ce
jianzhnie <jianzhnie@126.com>
2024-09-10 15:15:53 +0800

    Update docker

--- docker/docker_run3.sh
+++ docker/docker_run3.sh
@@ -1,17 +1,13 @@
 # Change `device` upon your resources
-docker_images=cybersim:v1
-
-model_dir=/home/niejz/work_dir/
+docker_images=cybersim:latest
 
 docker run -it -u root --ipc=host --net=host \
-        --name niejzh_PleaseDontStop \
-        --device=/dev/davinci6 \
+        --device=/dev/davinci7 \
         --device=/dev/davinci_manager \
         --device=/dev/devmm_svm \
         --device=/dev/hisi_hdc \
         -v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
         -v /usr/local/Ascend/add-ons/:/usr/local/Ascend/add-ons/ \
         -v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi \
-        -v ${model_dir}:${model_dir} \
         -v /var/log/npu:/usr/slog ${docker_images} \
         /bin/bash

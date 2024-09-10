855b21acb6f75f41b8c9b4411e7612d86237979d
jianzhnie <jianzhnie@126.com>
2024-09-10 18:34:15 +0800

    Update

--- examples/cyberattacksim/run_default_18_nodes_env.py
+++ examples/cyberattacksim/run_default_18_nodes_env.py
@@ -161,7 +161,6 @@ def main() -> None:
     # define callback to stop the trainingX
     stop_train_callback = StopTrainingOnNoModelImprovement(
         max_no_improvement_evals=5, min_evals=10, verbose=1)
-    print(stop_train_callback)
     eval_callback = EvalCallback(
         env,
         n_eval_episodes=10,

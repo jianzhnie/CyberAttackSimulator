741a1ba5f1e9d8b8d0ddca3f4d972a8c31b2388d
jianzhnie <jianzhnie@126.com>
2024-10-09 10:08:06 +0800

    Update cybersim

--- docker/docker_run2.sh
+++ docker/docker_run2.sh
@@ -1,5 +1,5 @@
 # Change `device` upon your resources
-docker_images=cybersim:v1
+docker_images=cybersim:v2
 
 model_dir=/home/niejz/work_dir/
 
@@ -10,8 +10,11 @@ docker run -it -u root --ipc=host --net=host \
         --device=/dev/devmm_svm \
         --device=/dev/hisi_hdc \
         -v /usr/local/Ascend/driver:/usr/local/Ascend/driver \
+        -v /usr/local/dcmi:/usr/local/dcmi   \
         -v /usr/local/Ascend/add-ons/:/usr/local/Ascend/add-ons/ \
         -v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi \
+        -v /usr/local/sbin/npu-smi:/usr/local/sbin/npu-smi   \
+        -v /usr/local/Ascend/firmware:/usr/local/Ascend/firmware  \
         -v ${model_dir}:${model_dir} \
         -v /var/log/npu:/usr/slog ${docker_images} \
         /bin/bash

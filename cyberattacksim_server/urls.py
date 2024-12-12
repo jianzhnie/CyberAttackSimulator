a0189306e54218e50d2fd6e1b2436733878eb886
JianZhengNie <jianzhnie@126.com>
2024-12-12 16:03:15 +0800

    Update cyberattacksim server

--- cyberattacksim_server/urls.py
+++ cyberattacksim_server/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 
+from cyberattacksim_gui.views.app_view import AppView
 from cyberattacksim_gui.views.game_mode_config_view import GameModeConfigView
 from cyberattacksim_gui.views.game_modes_view import GameModesView
 from cyberattacksim_gui.views.home_view import HomeView
-from cyberattacksim_gui.views.main_view import MainView
+from cyberattacksim_gui.views.marl_view import MarlView
 from cyberattacksim_gui.views.network_creator_view import NetworkCreator
 from cyberattacksim_gui.views.network_editor_view import NetworkEditor
+from cyberattacksim_gui.views.network_view import NetWorkView
 from cyberattacksim_gui.views.networks_view import NetworksView
+from cyberattacksim_gui.views.platform_view import PlatformView
 from cyberattacksim_gui.views.run_view import RunView
 from cyberattacksim_gui.views.utils.update_network_layout import \
     update_network_layout
 from cyberattacksim_gui.views.utils.utils import (db_manager, get_output,
                                                   update_game_mode)
+from cyberattacksim_gui.views.vrl_view import VrlView
 
 # 这段代码是一个Django项目中的URL配置模块，它定义了项目中不同的URL路径及其对应的视图。
 
@@ -24,8 +28,12 @@ from cyberattacksim_gui.views.utils.utils import (db_manager, get_output,
 # - path('docs/', DocsView.as_view(), name='docs')：当访问/docs/时，调用DocsView视图。
 
 urlpatterns = [
-    path('', MainView.as_view(), name='Main'),
-    path('home/', HomeView.as_view(), name='Home'),
+    path('', HomeView.as_view(), name='Home'),
+    path('network/', NetWorkView.as_view(), name='Network'),
+    path('vrl/', VrlView.as_view(), name='Vrl'),
+    path('marl/', MarlView.as_view(), name='Marl'),
+    path('app/', AppView.as_view(), name='App'),
+    path('platform/', PlatformView.as_view(), name='PlatForm'),
     path('run/', RunView.as_view(), name='Run session'),
     path('game_modes/', GameModesView.as_view(), name='Manage game modes'),
     path('networks/', NetworksView.as_view(), name='Manage networks'),

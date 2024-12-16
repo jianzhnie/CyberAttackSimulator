0dad711c16ffd69f03c934b40e2f7e12630bfe54
JianZhengNie <jianzhnie@126.com>
2024-12-16 17:34:11 +0800

    Update cyberattacksim

--- cyberattacksim/envs/generic/core/action_loops.py
+++ cyberattacksim/envs/generic/core/action_loops.py
@@ -146,7 +146,7 @@ class ActionLoop:
                     os.makedirs(webm_output_directory)
                 webm_path = os.path.join(
                     webm_output_directory,
-                    f'{self.filename}_{string_time}_{self.episode_count}.mp4',
+                    f"{self.filename}_{string_time}_{self.episode_count}.webm",
                 )
 
                 # video generator thread
